f12364f3ffdacc82baebba42fb88507dd320087b
Dennis <pobaby1125@qq.com>
2019-06-25 00:20:00 +0800

    创建项目模态框和视图文件的引入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/5.8.2/css/all.min.css">
+
 </head>
 <body>
     <div id="app">
